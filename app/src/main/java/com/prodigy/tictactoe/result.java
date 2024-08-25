93e5a07d47de063a3afba9382860239b511c692a
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-08-25 12:47:23 +0530

    initial

--- app/src/main/java/com/prodigy/tictactoe/result.java
+++ app/src/main/java/com/prodigy/tictactoe/result.java
@@ -20,5 +20,6 @@ public class result extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
