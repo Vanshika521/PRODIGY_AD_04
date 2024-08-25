376492c8fadfc01c79d759b95895128ee77c1a1a
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-08-25 23:16:43 +0530

    Players Name And Designing

---
+++ app/src/main/java/com/prodigy/tictactoe/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.prodigy.tictactoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Animation top,bottom;
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        img = findViewById(R.id.image);
+
+        top = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+
+        img.setAnimation(top);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,players.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
