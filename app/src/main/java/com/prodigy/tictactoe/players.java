6709ffc00c50a1c0bf9a62f07063a0eb57efe68b
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-08-26 22:07:12 +0530

    Players Name And Designing

--- app/src/main/java/com/prodigy/tictactoe/players.java
+++ app/src/main/java/com/prodigy/tictactoe/players.java
@@ -1,6 +1,11 @@
 package com.prodigy.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +15,38 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class players extends AppCompatActivity {
 
+    EditText first,second;
+    Button start;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_players);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
 
+        first = findViewById(R.id.first);
+        second = findViewById(R.id.second);
+        start = findViewById(R.id.start);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String getFirst = first.getText().toString();
+                String getSecond = second.getText().toString();
+
+                if (getFirst.isEmpty() || getSecond.isEmpty() )
+                {
+                    Toast.makeText(players.this,"Enter Player's Name",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Intent intent = new Intent(players.this, MainActivity.class);
+                    intent.putExtra("First",getFirst);
+                    intent.putExtra("Second",getSecond);
+                    startActivity(intent);
+                }
+                }
         });
+
     }
 }
