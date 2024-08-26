8af6720925557fcb0f7a047dba2e561c88d707d5
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-08-26 23:15:04 +0530

    updated design

--- app/src/main/java/com/prodigy/tictactoe/result.java
+++ app/src/main/java/com/prodigy/tictactoe/result.java
@@ -1,25 +1,48 @@
 package com.prodigy.tictactoe;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class result extends AppCompatActivity {
+public class result extends Dialog {
 
+    private final String msg;
+    private final MainActivity mainActivity;
+
+    public result(@NonNull Context context,String msg, MainActivity mainActivity) {
+        super(context);
+        this.msg = msg;
+        this.mainActivity = mainActivity;
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_result);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main);
+
+        TextView message = findViewById(R.id.msg);
+        Button again = findViewById(R.id.btn);
+
+        message.setText(msg);
+
+        again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             //   mainActivity.restartMatch();
+                dismiss();
+            }
         });
+
 
     }
 }
